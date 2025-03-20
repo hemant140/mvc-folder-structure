3db3b8f9b0bc481bde26ecf32e8f16efd2e8e523
hemant140 <hemo.hm576@gmail.com>
2025-03-20 16:54:20 +0530

    structure completed

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import { globalErrorHandler, notFoundHandler } from './error/error-handler.js';
+import router from './routes.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 9000
 
+console.log(process.env.PORT, "PORT")
+
 const app = express();
 
+//Database Connection
+connectDB();
+
 app.use(express.json())
+
+app.use(router)
 
 app.get('/', (req, res) => {
     res.json({ status: true, message: `Server is running port: ${PORT}` })
 })
+
+// Handle 404 API Not Found
+app.use(notFoundHandler);
+// Global Error Handler
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running port: ${PORT}`)
